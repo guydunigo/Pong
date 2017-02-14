c021301366978baf12a62acc6400e726e819cddd
Guillaume GONI <gdn3@live.fr>
2017-02-14 23:30:27 +0100

    fixing some collisions with rectangles but some remains (wall left->top + top->right)

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -53,6 +53,7 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
     Circle *other;
     Rectangle *rec;
     bool collision = false;
+	Segment* seg = NULL;
 
     gravity[0] = gravityX;
     gravity[1] = gravityY;
@@ -64,63 +65,64 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 		norm[i] = tang[i] = 0;
     }
     alpha += rotZ*dt*360/(3.14159*2);
-    if (alpha > 360)
-	alpha -= 360;
-    if (alpha < -360)
-	alpha += 360;
+    if (alpha > 360) alpha -= 360;
+    if (alpha < -360) alpha += 360;
 
     // Bouncing against the walls
+	/*
     if (new_pos[0] < 0 || new_pos[0] + radius * 2 > width || new_pos[1] < 0 || new_pos[1] + radius * 2 > height)
     {
-	if (new_pos[0] < 0)
-	{ // Left
-	    norm[0] = 1;
-	    norm[1] = 0; // tang = [0,-1]
-	    new_pos[0] = 0;
-	    //new_cel[1] = (2*radius*new_rotZ + 5*new_cel[1])/7;
-	}
-	else if (new_pos[0] + radius * 2 > width)
-	{ // Right
-	    norm[0] = -1;
-	    norm[1] = 0; // tang = [0, 1]
-	    new_pos[0] = width - radius * 2;
-	    //new_cel[1] = (-2*radius*new_rotZ + 5*new_cel[1])/7;
-	}
-	else
-	{
-	    norm[0] = 0;
-	    norm[1] = 0;
-	}
-	if (new_pos[1] < 0)
-	{ // Top
-	    norm[0] += 0;
-	    norm[1] += 1; // tang = [1, 0]
-	    new_pos[1] = 0;
-	    //new_cel[0] = (-2*radius*new_rotZ + 5*new_cel[0])/7;
-	}
-	else if (new_pos[1] + radius * 2 > height)
-	{ // Bottom
-	    norm[0] += 0;
-	    norm[1] += -1; // tang = [-1,0]
-	    new_pos[1] = height - radius * 2;
-	    //new_cel[0] = (2*radius*new_rotZ + 5*new_cel[0])/7;
-	}
+		if (new_pos[0] < 0)
+		{ // Left
+			norm[0] = 1;
+			norm[1] = 0; // tang = [0,-1]
+			new_pos[0] = 0;
+			//new_cel[1] = (2*radius*new_rotZ + 5*new_cel[1])/7;
+		}
+		else if (new_pos[0] + radius * 2 > width)
+		{ // Right
+			norm[0] = -1;
+			norm[1] = 0; // tang = [0, 1]
+			new_pos[0] = width - radius * 2;
+			//new_cel[1] = (-2*radius*new_rotZ + 5*new_cel[1])/7;
+		}
+		else
+		{
+			norm[0] = 0;
+			norm[1] = 0;
+		}
+		if (new_pos[1] < 0)
+		{ // Top
+			norm[0] += 0;
+			norm[1] += 1; // tang = [1, 0]
+			new_pos[1] = 0;
+			//new_cel[0] = (-2*radius*new_rotZ + 5*new_cel[0])/7;
+		}
+		else if (new_pos[1] + radius * 2 > height)
+		{ // Bottom
+			norm[0] += 0;
+			norm[1] += -1; // tang = [-1,0]
+			new_pos[1] = height - radius * 2;
+			//new_cel[0] = (2*radius*new_rotZ + 5*new_cel[0])/7;
+		}
 
-	getTang(norm, tang);
+		getTang(norm, tang);
 
-	new_rotZ = (2 * radius * new_rotZ - 5 * (new_cel[0] * tang[0] + new_cel[1] * tang[1])) / (7 * radius);
-	a = new_cel[0] * norm[0] + new_cel[1] * norm[1];
-	for (i = 0; i < 2; i++)
-	{
-	    new_cel[i] = (-1) * BOUNCE_COEF * a * norm[i] - rotZ * radius * tang[i];
-	}
+		new_rotZ = (2 * radius * new_rotZ - 5 * (new_cel[0] * tang[0] + new_cel[1] * tang[1])) / (7 * radius);
+		a = new_cel[0] * norm[0] + new_cel[1] * norm[1];
+		for (i = 0; i < 2; i++)
+		{
+			new_cel[i] = (-1) * BOUNCE_COEF * a * norm[i] - rotZ * radius * tang[i];
+		}
     }
+	*/
 
     // Bouncing against Rectangles
     for (unsigned int i = 0; i < rects.size(); i++)
     {
 	rec = rects[i];
 	collision = false;
+	norm[0] = 0; norm[1] = 0;
 	// Detect if in the rect
 	// Components of the vector from the ball to the other one :
 	a = new_pos[0] + radius - rec->getX() - rec->getWidth() / 2;
@@ -133,28 +135,28 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 	    // Collision on a (moving) plane
 	    if (new_pos[1] + 2 * radius > rec->getY() && new_pos[1] < rec->getY())
 	    {
-		collision = true;
-		// Top collision
-		norm[0] += 0;
-		norm[1] += -1; // tang = [-1,0]
-		new_pos[1] = rec->getY() - 2 * radius;
-		//new_cel[0] = (2*radius*new_rotZ + 5*new_cel[0])/7;
+			collision = true;
+			// Top collision
+			norm[0] = 0;
+			norm[1] = -1; // tang = [-1,0]
+			new_pos[1] = rec->getY() - 2 * radius;
+			//new_cel[0] = (2*radius*new_rotZ + 5*new_cel[0])/7;
 	    }
 	    else if (new_pos[1] + 2 * radius > rec->getY() + rec->getHeight() && new_pos[1] < rec->getY() + rec->getHeight())
 	    {
-		collision = true;
-		// Bottom collision
-		norm[0] += 0;
-		norm[1] += 1; // tang = [1, 0]
-		new_pos[1] = rec->getY() + rec->getHeight();
-		//new_cel[0] = (-2*radius*new_rotZ + 5*new_cel[0])/7;
+			collision = true;
+			// Bottom collision
+			norm[0] = 0;
+			norm[1] = 1; // tang = [1, 0]
+			new_pos[1] = rec->getY() + rec->getHeight();
+			//new_cel[0] = (-2*radius*new_rotZ + 5*new_cel[0])/7;
 	    }
 	}
 	// Horizontal collision :
 	else if (new_pos[1] + radius > rec->getY() && new_pos[1] + radius < rec->getY() + rec->getHeight())
 	{
 	    // Collision on a (moving) plane
-	    if (new_pos[0] + 2 * radius > rec->getY() && new_pos[0] < rec->getY())
+	    if (new_pos[0] + 2 * radius > rec->getX() && new_pos[0] < rec->getX())
 	    {
 			collision = true;
 			// Left collision
@@ -163,7 +165,7 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 			new_pos[0] = rec->getX() - radius * 2;
 			//new_cel[1] = (-2*radius*new_rotZ + 5*new_cel[1])/7;
 	    }
-	    else if (new_pos[0] + 2 * radius > rec->getY() + rec->getHeight() && new_pos[0] < rec->getY() + rec->getHeight())
+	    else if (new_pos[0] + 2 * radius > rec->getX() + rec->getWidth() && new_pos[0] < rec->getX() + rec->getWidth())
 	    {
 			collision = true;
 			// Right collision
@@ -184,7 +186,6 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 			if (c < radius)
 			{
 				collision = true;
-				std::cout << "Top right corner collision" << std::endl;
 			}
 	    }
 	    else if ((a > 0 && b > 0))
@@ -196,7 +197,6 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 			if (c < radius)
 			{
 				collision = true;
-				std::cout << "Bottom right corner collision" << std::endl;
 			}
 	    }
 	    else if ((a < 0 && b < 0))
@@ -208,7 +208,6 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 			if (c < radius)
 			{
 				collision = true;
-				std::cout << "Top left corner collision" << std::endl;
 			}
 	    }
 	    else if ((a < 0 && b > 0))
@@ -220,16 +219,14 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 			if (c < radius)
 			{
 				collision = true;
-				std::cout << "Bottom left corner collision" << std::endl;
 			}
 	    }
 		if (collision)
 		{
-			norm[0] += a / c;
-			norm[1] += b / c;
+			norm[0] = a / c;
+			norm[1] = b / c;
 			new_pos[0] += norm[0];
 			new_pos[1] += norm[1];
-			std::cout << norm[0] << "\t" << norm[1] << std::endl;
 		}
 	}
 
@@ -240,8 +237,7 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 	    a = new_cel[0] * norm[0] + new_cel[1] * norm[1];
 	    for (i = 0; i < 2; i++)
 	    {
-		new_pos[i] += norm[i];
-		new_cel[i] = (-1) * BOUNCE_COEF * a * norm[i] - rotZ * radius * tang[i];
+			new_cel[i] = (-1) * BOUNCE_COEF * a * norm[i] - rotZ * radius * tang[i];
 	    }
 	}
     }
@@ -262,20 +258,20 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
 	    // If the 2 balls are colliding
 	    if (d < 0)
 	    {
-		norm[0] = a / c;
-		norm[1] = b / c;
-		getTang(norm, tang);
-		a = new_cel[0];
-		b = new_cel[1];
-		c = (mass - massO) / (mass + massO);
-		d = 2 / (mass + massO);
-		new_cel[0] = (c * new_cel[0] + massO * d * other->getCelX()) * BOUNCE_COEF;
-		new_cel[1] = (c * new_cel[1] + massO * d * other->getCelY()) * BOUNCE_COEF;
-		other->setCelX((-c * other->getCelX() + mass * d * a) * BOUNCE_COEF);
-		other->setCelY((-c * other->getCelY() + mass * d * b) * BOUNCE_COEF);
-		new_pos[0] += norm[0];
-		new_pos[1] += norm[1];
-		other->move(-norm[0], -norm[1]);
+			norm[0] = a / c;
+			norm[1] = b / c;
+			getTang(norm, tang);
+			a = new_cel[0];
+			b = new_cel[1];
+			c = (mass - massO) / (mass + massO);
+			d = 2 / (mass + massO);
+			new_cel[0] = (c * new_cel[0] + massO * d * other->getCelX()) * BOUNCE_COEF;
+			new_cel[1] = (c * new_cel[1] + massO * d * other->getCelY()) * BOUNCE_COEF;
+			other->setCelX((-c * other->getCelX() + mass * d * a) * BOUNCE_COEF);
+			other->setCelY((-c * other->getCelY() + mass * d * b) * BOUNCE_COEF);
+			new_pos[0] += norm[0];
+			new_pos[1] += norm[1];
+			other->move(-norm[0], -norm[1]);
 	    }
 	}
     }
@@ -283,8 +279,8 @@ void Circle::step(float dt, float gravityX, float gravityY, std::vector<Rectangl
     // Nullify the celerity when too low
     for (i = 0; i < 2; i++)
     {
-	if (new_cel[i] < CELERITY_LIM && new_cel[i] > -CELERITY_LIM)
-	    new_cel[i] = 0;
+		if (new_cel[i] < CELERITY_LIM && new_cel[i] > -CELERITY_LIM)
+			new_cel[i] = 0;
     }
 
     // Actually update values
